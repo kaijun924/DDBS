8bd64bf488cdb0d3e9f9d47f2761906a8aba0d2d
kaijun924 <kaijun823@gmail.com>
2024-12-15 13:39:59 +0800

    change the insert logic of be_read

--- database/handler.py
+++ database/handler.py
@@ -1,5 +1,6 @@
-from base_handler import MongoDBHandler, TableHandler
+from base_handler import MongoDBHandler, TableHandler, handle_exceptions, log_execution_time
 from handler_tools import BeReadTools, ReadTime, DateToTimestamp
+import time
 cache = {}
 
 class UserTableHandler(TableHandler):
@@ -57,6 +58,21 @@ class ArticleTableHandler(TableHandler):
             pipeline.append({"$limit": count})
 
         articles = self.collection.aggregate(pipeline)
+        return list(articles)
+    
+    def fetch_article_for_beread(self, conditions={},offset=0):
+        fields = {
+                    "_id": 0,
+                    "timestamp": 0,
+                    "title": 0,
+                    "abstract": 0,
+                    "articleTags": 0,
+                    "authors": 0,
+                    "language": 0,
+                    "text": 0,
+                    "image": 0,
+                }
+        articles = self.collection.find(conditions, fields).skip(offset)
         return list(articles)
     
     def fetch_articles_by_category(self, category: str, count=100, offset=0):
@@ -176,6 +192,7 @@ class BeReadTableHandler(TableHandler):
         super().__init__(db_handler.be_read_collection)
         # self.userTableHandler = UserTableHandler(db_handler)
         self.readTableHandler = ReadTableHandler(db_handler)
+        self.articleTableHandler = ArticleTableHandler(db_handler)
         self.tools = BeReadTools()
     
     def _process_record(self, record):
@@ -186,6 +203,69 @@ class BeReadTableHandler(TableHandler):
         self.collection.insert_many(record) #
     
     ##或许可以重写bulk_insert
+    @handle_exceptions
+    @log_execution_time
+    def bulk_insert_be_read(self, batch_size = 5000):
+        articles = self.articleTableHandler.fetch_article_for_beread({}, 0)
+        buffer = []
+        mapping = {}
+        for article in articles:
+            be_read_entity = {}
+            be_read_entity["id"] = "br" + article["aid"]
+            be_read_entity["timestamp"] = str(time.time())
+            be_read_entity["category"] = article["category"]
+            be_read_entity["aid"] = article["aid"]
+
+            mapping[article["aid"]] = len(buffer)
+            buffer.append(be_read_entity)
+        
+        print(f"Finished processing {len(buffer)} records.")
+
+        reads = self.readTableHandler.fetch_reads_to_beread({}, None, None)
+        
+        count = 0
+        for read in reads:
+            aid = read["aid"]
+            if aid not in mapping:
+                continue
+            idx = mapping[aid]
+            buffer[idx]["readNum"] = buffer[idx].get("readNum", 0) + 1
+            buffer[idx]["readUidList"] = buffer[idx].get("readUidList", [])
+            buffer[idx]["readUidList"].append(read["uid"])
+            if read["commentOrNot"] == "1":
+                buffer[idx]["commentNum"] = buffer[idx].get("commentNum", 0) + 1
+                buffer[idx]["commentUidList"] = buffer[idx].get("commentUidList", [])
+                buffer[idx]["commentUidList"].append(read["uid"])
+            if read["agreeOrNot"] == "1":
+                buffer[idx]["agreeNum"] = buffer[idx].get("agreeNum", 0) + 1
+                buffer[idx]["agreeUidList"] = buffer[idx].get("agreeUidList", [])
+                buffer[idx]["agreeUidList"].append(read["uid"])
+            if read["shareOrNot"] == "1":
+                buffer[idx]["shareNum"] = buffer[idx].get("shareNum", 0) + 1
+                buffer[idx]["shareUidList"] = buffer[idx].get("shareUidList", [])
+                buffer[idx]["shareUidList"].append(read["uid"])
+            
+            count += 1
+            if count % 5000 == 0:
+                print(f"Processing {count} records.")
+        
+        insert_buffer = []
+        for be_read_entity in buffer:
+            if be_read_entity.get("category") == "science":
+                r1 = be_read_entity.copy()
+                r2 = be_read_entity.copy()
+                r1["shardCopy"] = 1
+                r2["shardCopy"] = 2
+                insert_buffer.extend([r1, r2])
+            else:
+                insert_buffer.append(be_read_entity)
+
+        self.collection.insert_many(insert_buffer)
+        print(f"Finished processing {count} records.")
+
+
+
+        
 
     def fetch_beReads(self, conditions={}, count=100, offset=0):
         pipeline = [
